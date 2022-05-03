607e8e72dea4abc72db5c116aedc725e8085bfdc
jspzyhl <jspzyhl@gmail.com>
2022-05-04 00:38:01 +0800

    541

--- db_init.sql
+++ db_init.sql
@@ -4,6 +4,6 @@ create table if not exists user_info(uid varchar(20) not null,nickname varchar(2
 create table if not exists user_cfg(id int not null auto_increment,last_uid varchar(20),primary key(id));
 create table if not exists wechat_bind(uid varchar(20) not null,openid varchar(32),primary key(uid));
 create table if not exists wechat_privilege(openid varchar(32) not null,admin int, primary key(openid));
-create table if not exists wechat_token(id int not null auto_increment,token varchar(200),expire_time float,primary key(id));
+create table if not exists wechat_token(id int not null auto_increment,token varchar(200),expire_time double,primary key(id));
 replace into user_info values(0,"default",null,null,null);
 insert ignore into user_cfg values(1,"0");
