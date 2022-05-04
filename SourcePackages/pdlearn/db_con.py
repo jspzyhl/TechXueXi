594900da0ce5bd79cc71d6af9f8e2bddf85cae00
jspzyhl <jspzyhl@gmail.com>
2022-05-04 21:26:50 +0800

    545

--- SourcePackages/pdlearn/db_con.py
+++ SourcePackages/pdlearn/db_con.py
@@ -2,6 +2,29 @@ import os
 import pymysql
 from pymysql.cursors import *
 from dbutils.pooled_db import PooledDB
+import socket
+import time
+
+
+def wait_for_port(port, host='localhost', timeout=5.0):
+    """Wait until a port starts accepting TCP connections.
+    Args:
+        port (int): Port number.
+        host (str): Host address on which the port should exist.
+        timeout (float): In seconds. How long to wait before raising errors.
+    Raises:
+        TimeoutError: The port isn't accepting connection after time specified in `timeout`.
+    """
+    start_time = time.perf_counter()
+    while True:
+        try:
+            with socket.create_connection((host, port), timeout=timeout):
+                break
+        except OSError as ex:
+            time.sleep(0.01)
+            if time.perf_counter() - start_time >= timeout:
+                raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
+                                   'connections.'.format(port, host)) from ex
 
 
 class DB:
@@ -14,6 +37,7 @@ class DB:
     @classmethod
     def init(cls, mincached_: int = 2):
         if not cls.__con_pool:
+            wait_for_port(3306, 'localhost', 60)
             cls.__con_pool = PooledDB(creator=pymysql,
                                       mincached=mincached_,
                                       ping=0,
