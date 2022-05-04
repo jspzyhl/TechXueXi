b28b6047756811559cffee99d97fb26e7b7a0a6d
jspzyhl <jspzyhl@gmail.com>
2022-05-04 20:50:38 +0800

    544

--- run_mysql.sh
+++ run_mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# /usr/sbin/mysqld --user=root --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --pid-file=/run/mysqld/mysqld.pid > /dev/null & 
-# wait-for-it -t 0 localhost:3306
-/etc/init.d/mysql start
+/usr/sbin/mysqld --user=root --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mysql/plugin --pid-file=/run/mysqld/mysqld.pid > /dev/null & 
+wait-for-it -t 0 localhost:3306
+# /etc/init.d/mysql start
